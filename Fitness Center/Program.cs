639b6bc06117f7328a2b229ee24139035d156bf1
Irving Kirvan <kirvanirving@gmail.com>
2023-10-19 10:34:08 -0400

    Initial Commit

---
+++ Fitness Center/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fitness_Center
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
