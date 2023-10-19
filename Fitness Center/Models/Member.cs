b7c25ce81067f15786b3b5c0213385585051c948
Alex Young <alexanderjyoung15@gmail.com>
2023-10-19 16:38:52 -0400

    bones

---
+++ Fitness Center/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Center
+{
+    public abstract class Member
+    {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public abstract void CheckIn();
+    
+
+
+    }
+}
