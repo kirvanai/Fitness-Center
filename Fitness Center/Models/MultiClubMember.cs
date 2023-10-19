0eea8b1443fa59921c86e83e23b0b561abaa9aa5
Michael Townley <birdie4mt@hotmail.com>
2023-10-19 15:53:34 -0400

    Base Code

---
+++ Fitness Center/Models/MultiClubMember.cs
@@ -0,0 +1,28 @@
+﻿namespace Fitness_Center.Models
+{
+    public class MultiClubMember : Member
+    {
+        public int PointsEarned { get; set; }
+
+        public List<Club> clubs { get; set; }
+
+        public MultiClubMember(int id, string name) : base(id, name)
+        {
+            ID = id;
+            Name = name;
+
+        }
+
+        public override void CheckIn(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CalculateFee()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
