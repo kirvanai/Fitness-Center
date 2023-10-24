c1f8833b404dbf24b79062be751edc0cf266a7d1
Michael Townley <birdie4mt@hotmail.com>
2023-10-24 14:44:49 -0400

    Test bin

--- Fitness Center/Models/Club.cs
+++ Fitness Center/Models/Club.cs
@@ -23,7 +23,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public string City { get; set; }
+        public string City { get; set; } 
         public string State { get; set; }
         public int ZipCode { get; set; }
         public List<Facilities> ClubFacilities { get; set; }
